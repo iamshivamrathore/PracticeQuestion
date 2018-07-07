03b5c3c65666609bf6ca00558dc46bec643c50d8
Shivam Rathore <r.shivam1993@gmail.com>
2018-07-07 13:28:01 +0530

    Commit 1

---
+++ src/algorithms/AppleAndOrange.java
@@ -0,0 +1,35 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class AppleAndOrange {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int s = in.nextInt();
+		int t = in.nextInt();
+		int a = in.nextInt();
+		int b = in.nextInt();
+		int m = in.nextInt();
+		int n = in.nextInt();
+		int[] apple = new int[m];
+		in.close();
+		int app_count = 0;
+		for (int apple_i = 0; apple_i < m; apple_i++) {
+			apple[apple_i] = in.nextInt();
+			if (a + apple[apple_i] >= s && a + apple[apple_i] <= t) {
+				app_count++;
+			}
+		}
+		int[] orange = new int[n];
+		int orange_count = 0;
+		for (int orange_i = 0; orange_i < n; orange_i++) {
+			orange[orange_i] = in.nextInt();
+			if (b + orange[orange_i] >= s && b + orange[orange_i] <= t) {
+				orange_count++;
+			}
+		}
+		System.out.println(app_count);
+		System.out.println(orange_count);
+	}
+}
---
+++ src/algorithms/Staircase.java
@@ -0,0 +1,25 @@
+package algorithms;
+
+import java.util.Scanner;
+
+public class Staircase {
+
+	public static void main(String[] args) {
+		/*
+		 * Enter your code here. Read input from STDIN. Print output to STDOUT. Your
+		 * class should be named Solution.
+		 */
+		Scanner s = new Scanner(System.in);
+		int n = s.nextInt();
+
+		for (int i = 1; i <= n; i++) {
+			for (int j = i; j < n; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 1; k <= i; k++) {
+				System.out.print("#");
+			}
+			System.out.println();
+		}
+	}
+}
